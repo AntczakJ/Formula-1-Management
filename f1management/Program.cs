a954eb27d6410cc30c5f3affc99ada1d34be48ea
AntczakJ <145098678+AntczakJ@users.noreply.github.com>
2025-03-16 07:48:07 +0100

    Add files via upload

---
+++ f1management/Program.cs
@@ -0,0 +1,11 @@
+﻿using f1management.classes;
+
+namespace f1management;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Principal Principal1 = new Principal("John", "Smith");
+    }
+}
---
+++ f1management/Team Management/Driver.cs
@@ -0,0 +1,12 @@
+﻿namespace f1management.classes;
+
+public class Driver : Team
+{
+    public string Driver_FirstName { get; set; }
+    public string Driver_LastName { get; set; }
+    
+    public void PrincipalInfo(string FirstName, string LastName)
+    {
+        Console.WriteLine($"nazywa sie {FirstName} {LastName}");
+    }
+}
